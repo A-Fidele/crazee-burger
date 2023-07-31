b1f79c8a4259f3a1d1b1fa3e5d222faf47c2d893
Aurelien <groggy_doctor@hotmail.com>
2023-07-31 18:26:53 +0200

    F02: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
-import LoginPage from './components/pages/login/LoginPage'
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './components/pages/login/LoginPage';
+import OrderPage from './components/pages/order/OrderPage';
+import ErrorPage from './components/pages/error/ErrorPage';
+
 
 function App() {
-  
+
   return (
-    <LoginPage/>      
+    <Routes>
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/order/:username' element={<OrderPage />} />
+      <Route path='*' element={<ErrorPage />} />
+    </Routes>
+
   )
 }
 
-export default App
+export default App;
