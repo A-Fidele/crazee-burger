57cfa10bd038edbb8083d4e71ad3361a9dbe994e
A-Fidele <groggy_doctor@hotmail.com>
2023-08-26 21:53:35 +0200

    F07: add panel, ajouter-modifier, add react-icons

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,11 +1,22 @@
 import styled from "styled-components"
 import { theme } from "../../../../theme"
 import Menu from "./Menu"
+import { FiChevronUp } from 'react-icons/fi'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { MdModeEditOutline } from 'react-icons/md'
+
 
 export default function Main() {
   return (
     <MainStyled className="main">
-      {/* <div className="basket">Basket</div> */}
+      <div className="panel-container">      
+        <div className="panel-option">
+          <button className="up-down-icon"><FiChevronUp /></button>
+          <button className="ajouter-modifier"><AiOutlinePlus /> Ajouter un produit</button>
+          <button className="ajouter-modifier"><MdModeEditOutline /> Modifier un produit</button>
+        </div>
+        <div className="panel">PANEL</div>
+    </div>
       <Menu />
     </MainStyled>
   )
@@ -21,9 +32,40 @@ const MainStyled = styled.div`
 
   display: grid;
   grid-template-columns: 1fr;
-
   overflow-y: scroll;
-  /* .basket {
-    background: pink;
-  } */
-`
+
+  .panel-container {
+    grid-row: 2;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 1400px;
+  }
+
+  .panel-option {
+    background: rgba(255, 0, 0, opacity);
+    width: 100%;
+    height: 44px;
+  }
+
+  .up-down-icon {
+    width: 62px;
+    height: 43px;
+    margin-left: 71px;
+    background:  ${theme.colors.background_white};
+
+  }
+  
+  .ajouter-modifier {
+    height: 44px;
+    background:  ${theme.colors.background_white};
+
+  }
+  
+  .panel {
+    background:  ${theme.colors.background_white};
+    width: 100%;
+    height: 250px;
+  }
+
+  `
