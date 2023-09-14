948cdde04503c731fbab91396b8630337b3f7486
A-Fidele <groggy_doctor@hotmail.com>
2023-09-14 15:43:08 +0200

    F08bis>Panel>add layout add product

--- src/components/pages/order/Main/MainRightSide/Admin/Panel.jsx
+++ src/components/pages/order/Main/MainRightSide/Admin/Panel.jsx
@@ -3,6 +3,7 @@ import { theme } from "../../../../../../theme";
 import { useContext } from "react";
 import { getTabSelected, tabsConfig } from "./tabsConfig";
 import Usercontext from "../../../../../../context/UserContext";
+import TextInput from "../../../../../reusable-ui/TextInput";
 
 export default function Panel() {
   const { currentTabSelected } = useContext(Usercontext);
@@ -10,10 +11,19 @@ export default function Panel() {
   const tabs = tabsConfig;
 
   const tabSelected = getTabSelected(tabs, currentTabSelected);
+  const inputFields = tabSelected.infoProduct.map((fields) => {
+    return <TextInput Icon={fields.addIcon} label={tabSelected.label} />
+  })
 
   return (
     <AdminPanelStyled>
-      <p>{tabSelected && tabSelected.label}</p>
+      {/* <p>{tabSelected && tabSelected.label}</p> */}
+
+      <div className="add-container">
+        <div className="image-preview">imagepreview</div>
+        <div className="input-fields">{inputFields}</div>
+        <div className="submit-button">submitbutton</div>
+      </div>
     </AdminPanelStyled>
   );
 }
@@ -22,6 +32,34 @@ const AdminPanelStyled = styled.div`
   background: ${theme.colors.white};
   border: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
+
+.add-container {
+  width: 70%;
+  height: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+
+  border: 1px solid black;
+}
+
+.image-preview {
+  background-color: red;
+  grid-area: 1/1/4/2;
+
+}
+
+.input-fields {
+  background-color: blue;
+  grid-area: 1/2/4/2;
+  
+}
+
+.submit-button {
+  background-color: green;
+  grid-area: 4/2/5/3;
+}
 
   p {
     padding: 0 20px;
