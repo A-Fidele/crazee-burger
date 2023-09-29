0ae03b9d46fd5ce4ac6e435100f2b232f3f8b268
A-Fidele <groggy_doctor@hotmail.com>
2023-09-29 10:56:14 +0200

    F11>Update FakeMenu

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { fakeMenu2 } from "../fakeData/fakeMenu";
+import { LARGE } from "../fakeData/fakeMenu";
 import { deepClone } from "../utils/array";
 
 export const useMenu = () => {
-  const [menu, setMenu] = useState(fakeMenu2);
+  const [menu, setMenu] = useState(LARGE);
   const handleEdit = (productEdit) => {
     const menuCopy = deepClone(menu);
     const indexProduct = menu.findIndex((data) => data.id === productEdit.id);
@@ -24,7 +24,7 @@ export const useMenu = () => {
   };
 
   const resetMenu = () => {
-    setMenu(fakeMenu2);
+    setMenu(LARGE);
   };
 
   return {
